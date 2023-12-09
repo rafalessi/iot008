3ca8fd086fd9d4ed8bcc9ca16aa7413c70017a6e
Rafael Alessi Muntsch <rafalessi87@gmail.com>
2023-12-09 21:37:13 +0100

    Update RSA.py
    Incluso input para números primos

--- RSA.py
+++ RSA.py
@@ -2,7 +2,8 @@
 # Rafael Alessi Muntsch
 # rafalessi87@gmai.com
 
-# Codigo para RSA dado numeros primos 7 e 13 a serem utilizados na criptografia RSA de uma mensagem
+# Codigo para RSA dado numeros primos do usuario a serem utilizados na criptografia RSA de uma mensagem predefinida
+# Tem algum erro que se a mensagem for um numero maior que o produto dos numeros primos, ela nao eh corretamente criptografada.
 
 import random
 
@@ -24,8 +25,29 @@ def isPrime(n):
     f += 6
   return True
 
-p1 = 7 # Primeiro numero primo
-p2 = 13 # Segundo numero primo
+while True:
+    try:
+        p1 = int(input('Primeiro numero primo:'))
+        while isPrime(p1) != True:
+            print('Numero nao primo. Tente novamente! Primeiro numero primo:')
+            input_p1 = input()
+            p1 = int(input_p1)
+        break
+    except ValueError:
+        print('Numero nao valido. Tente novamente! Primeiro numero primo:')
+
+while True:
+    try:
+        p2 = int(input('Segundo numero primo:'))
+        while isPrime(p2) != True:
+            print('Numero nao primo. Tente novamente! Segundo numero primo:')
+            input_p2 = input()
+            p2 = int(input_p2)
+        break
+    except ValueError:
+        print('Numero nao valido. Tente novamente! Segundo numero primo:')
+
+
 print("Numeros primos selecionados:",p1,",",p2)
 
 n = p1*p2 # produto dos numero primos selecionados
@@ -41,10 +63,11 @@ pu1 = random.choice(primes) # selecionar alguns dos valores calculados aleatoria
 
 pr1_t = 1 # valor inicial de chave privada iniciar verificacao
 cont_inicial = 0
-cont_final = random.randint(1,10) # valor aleatorio para selecionar a x iteracao desse calculo (usei ate 10 para nao gerar calculos muito grandes)
+cont_final = random.randint(1,5) # valor aleatorio para selecionar a x iteracao desse calculo (usei ate 10 para nao gerar calculos muito grandes)
 # funcao para achar a primeira chave privada que atenda a restricao de (pu1*pr1)%z
 while True:
-    if (pu1*pr1_t)%z == 1:
+    mod = (pu1*pr1_t)%z
+    if mod == 1:
         if cont_inicial == cont_final:
             break        
         cont_inicial += 1
@@ -56,6 +79,8 @@ print("Chave Publica (PU) : ", pu1)
 print("Chave Privada (PR) : ", pr1)
 
 m = [80, 70, 20, 30] # mensagem
+
+
 print("Mensagem Enviada: ", m)
 m_crp = []
 m_dcrp = []
@@ -68,10 +93,10 @@ for i in m:
 print("Mensagem Encriptada: ", m_crp)
 
 # decodificar mensagem criptograda
-for i in m_crp:
-   i1 = pow(i,pr1)
-   i2 = i1%n
-   m_dcrp.append(i2)
+for j in m_crp:
+   j1 = pow(j,pr1)
+   j2 = j1%n
+   m_dcrp.append(j2)
 print("Mensagem Decriptada: ", m_dcrp)
 
 if m_dcrp == m:
